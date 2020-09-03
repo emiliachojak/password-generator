ba5df05ca746abbd97a75ee1aa80f953839ebbd7
Emilia Chojak <emilia.chojak@minebest.com>
2020-09-03 17:25:59 +0200

    Changed settings

--- password_generator/password_generator/settings.py
+++ password_generator/password_generator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pzo0xhe0_u7x!$n^=+zp(n8a0pj86)@=krf3(aii_08=e*^!*o'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
